9a8f3c18daab43f7fd5fd302e4866a0e04ec5f2b
Clemente Acosta <clems.acosta@gmail.com>
2022-08-10 10:53:31 -0500

    Add background image with media query

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 import { colors } from '../../styles/styleConfig'
+import mobileImg from '../../assets/images/pattern-background-mobile.svg'
+import desktopImg from '../../assets/images/pattern-background-desktop.svg'
 export const StyledContainer = styled.main`
+  background-image: url(${mobileImg});
   background-color: ${colors.light};
+  background-repeat: no-repeat;
+  background-size: contain;
   min-height: 100vh;
   display: grid;
   place-items: center;
+  
+  @media (min-width: 500px) {    
+    background-image: url(${desktopImg});
+  }
 `
